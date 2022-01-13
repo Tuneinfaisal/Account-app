d90a588e98d28e09c373d6869182f170e6666fed
Asif A Peerzade <80567371+TechStark-coder@users.noreply.github.com>
2022-01-13 11:40:57 +0530

    I did this commit to check guys
    asif here commiting now

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,9 @@ import React from "react";
 export default () => {
     return (
         <div class="text-center">
-            <h1>Welcome to Accounts app</h1>
+            <h1>Welcome to Accounts app...............</h1>
+            <h2>hey git</h2>
+            <h1> Faisal i did this commit on 13 Jan 2022</h1>
         </div>
     )
 }
