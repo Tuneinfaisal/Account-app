3e824c90f3d02432312d7b6a732b0f59e62347ad
Faisal <tuneinfaisal@gmail.com>
2022-01-13 15:26:16 +0530

    Balance field bug fixed in Register.js

--- src/Register.js
+++ src/Register.js
@@ -2,27 +2,13 @@ import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-async function loginUser(credentials) {
-    return fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    })
-        .then(data => data.json())
-}
-
 export default function Login({ setToken }) {
     const [username, setUserName] = useState();
+    const [balance, setBalance] = useState();
     const [password, setPassword] = useState();
+    
     const handleSubmit = async e => {
         e.preventDefault();
-        const token = await loginUser({
-            username,
-            password
-        });
-        setToken(token);
     }
     return (
         <>
@@ -38,8 +24,8 @@ export default function Login({ setToken }) {
                     <br/><br/>
                     <label>
                         <p>Balance</p>
-                        <input type="password" 
-                        onChange={e => setPassword
+                        <input type="text" 
+                        onChange={e => setBalance
                         (e.target.value)} />
                     </label>
                     <br/><br/>
