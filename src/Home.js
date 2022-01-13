913d959f8827410303ce5bc575519a9d01a60100
Asif A Peerzade <80567371+TechStark-coder@users.noreply.github.com>
2022-01-13 12:02:51 +0530

    home prchanges
    xcommit 4

--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ export default () => {
             <h1>Welcome to Accounts app...............</h1>
             <h2>hey git</h2>
             <h1> Faisal i did this commit on 13 Jan 2022</h1>
+            <h2>testing</h2>
         </div>
     )
 }
