3f8b9edaee5d7b8cea7591aa0413f3ba5f090cba
Faisal <tuneinfaisal@gmail.com>
2022-01-13 13:12:03 +0530

    Added fields required fields in register page

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export default () => {
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+export default function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
     return (
-        <div>Hello from Register!</div>
+        <>
+            <div className="login-wrapper">
+                <h1>Sign up</h1>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        <p>Name</p>
+                        <input type="text" 
+                        onChange={e => setUserName
+                        (e.target.value)} />
+                    </label>
+                    <br/><br/>
+                    <label>
+                        <p>Balance</p>
+                        <input type="password" 
+                        onChange={e => setPassword
+                        (e.target.value)} />
+                    </label>
+                    <br/><br/>
+                    <label>
+                        <p>Password</p>
+                        <input type="password" 
+                        onChange={e => setPassword
+                        (e.target.value)} />
+                    </label>
+                    <br/><br/>
+                    <div>
+                        {/* <button type="submit" onClick={Home}>Login</button> */}
+                        <Link to="/" className="btn btn-primary">Signup</Link>
+                    </div>
+                </form>
+            </div>
+            <div class="text-center">
+                <h1>Work in processss</h1>
+            </div></>
     )
+}
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
 }
