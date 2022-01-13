82a71e8437309e5bdfe6e406801f4126c3cd70a6
Asif A Peerzade <80567371+TechStark-coder@users.noreply.github.com>
2022-01-13 12:19:41 +0530

    Home pr chnages kiye hain..
    home app commit

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 
-export default () => {
+export default function home () {
     return (
-        <div class="text-center">
-            <h1>Welcome to Accounts app...............</h1>
-            <h2>hey git</h2>
-            <h1> Faisal i did this commit on 13 Jan 2022</h1>
-            <h2>testing</h2>
-        </div>
+        <><><div class="text-center">
+            <h1>Welcome to Dashboard</h1>
+        </div><div className="btns">
+                <button class="button-30" onClick={popMsg}>Passbook</button><br /><br />
+                {/* <button class="button-30" onClick={popTrans}>transaction</button> */}
+            </div></><div>
+            <Link to="/Login" className="button-17">Login out</Link>
+            <br/>
+    
+            
+            </div></>
     )
+    function popMsg()  {
+        alert('Your balance is..!');
+        //alter {balance} table
+      }
 }
