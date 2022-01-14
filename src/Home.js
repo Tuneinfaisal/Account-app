d8b75a878dd9b9ac67b0ed5f326b619b05151085
Faisal <tuneinfaisal@gmail.com>
2022-01-14 15:08:43 +0530

    Tracsaction page redirect to login

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,44 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-export default function home () {
+export default function home() {
+
+    // const tracsactionHandler = (e) => {
+    //     e.preventDefault();
+    //     return(
+    //             <Link to="./"></Link>
+    //     )
+    // }
+    
     return (
-        <><><div class="text-center">
-            <h1>Welcome to Dashboard</h1>
-        </div><div className="btns">
-                <button class="button-30" onClick={popMsg}>Passbook</button><br /><br />
-            </div></><div>
-                <Link to="/TranPage" className="button-30"> Transaction Page </Link>
-            <Link to="/Login" className="button-17">Log out</Link>
-            <br/>  
-            </div></>
-    )
-    function popMsg()  {
-        alert('Your balance is..!');
+        <>
+            <>
+                <div class="text-center">
+                    <h1>Welcome to Dashboard</h1>
+                </div>
+                <div className="btns">
+                    <button class="button-30" onClick={popMsg}>
+                        Passbook
+                    </button>
+                    <br />
+                    <br />
+                </div>
+            </>
+            <div>
+                {/* <button className="button-30" onClick={tracsactionHandler}>Transaction page</button> */}
+                <Link to="/" className="button-30" >
+                    {" "}
+                    Transaction Page{" "}
+                </Link>
+                <Link to="/" className="button-17">
+                    Log out
+                </Link>
+                <br />
+            </div>
+        </>
+    );
+    function popMsg() {
+        alert("Your balance is..!");
         //alter {balance} table
-      }
+    }
 }
