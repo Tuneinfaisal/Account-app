a4c5077f3ac45209c1e9b6a2c9c2103013f67529
Faisal <tuneinfaisal@gmail.com>
2022-01-16 11:08:41 +0530

    Registration is working

--- src/Register.js
+++ src/Register.js
@@ -1,59 +1,57 @@
 import React, { useState } from "react";
+import Axios from "axios";
 // import PropTypes from 'prop-types';
 // import { Link } from 'react-router-dom';
 
 export default function Login() {
-    const [username, setUserName] = useState();
+    const [name, setName] = useState();
     const [balance, setBalance] = useState();
     const [password, setPassword] = useState();
-    
+    const url = "http://localhost:8000/api/v1/account/registration"
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        // const name = e.target.username;
-        // if(name == "") {
-        //     alert("must be filled");
-        // }
-
-        //send this userdata to backend
-        const newEntry = {username,balance,password};
-    }
-
-    const buttonClickHandler = (e) => {
-        e.preventDefault();
+        Axios.post(url,{
+            name: name,
+            balance: balance,
+            password: password
+        })
+        name = "";
+        balance = "";
+        password = "";
         alert("Your data is saved");
     }
     return (
         <>
-            <div className="login-wrapper">
+            <div>
                 <h1>Sign up</h1>
-                <form name="registerForm" onSubmit={handleSubmit}>
-                    <label>
-                        <p>Name</p>
-                        <input type="text" 
-                        value={username}
-                        onChange={e => setUserName
-                        (e.target.value)} required />
-                    </label>
-                    <br/><br/>
-                    <label>
-                        <p>Balance</p>
-                        <input type="text" 
-                        value={balance}
-                        onChange={e => setBalance
-                        (e.target.value)} required />
-                    </label>
-                    <br/><br/>
-                    <label>
-                        <p>Password</p>
-                        <input type="password" 
-                        value={password}
-                        onChange={e => setPassword
-                        (e.target.value)} required />
-                    </label>
-                    <br/><br/>
+                <label>
+                    <p>Name</p>
+                    <input type="text" 
+                    onChange={e => setName
+                    (e.target.value)} value={name} required />
+                </label>
+                <br/><br/>
+                <label>
+                    <p>Balance</p>
+                    <input type="text" 
+                    onChange={e => setBalance
+                    (e.target.value)} 
+                    value={balance}
+                    required />
+                </label>
+                <br/><br/>
+                <label>
+                    <p>Password</p>
+                    <input type="password" 
                     
-                    <button className="btn btn-primary" type="submit" onClick={buttonClickHandler}>Submit</button>
-                </form>
+                    onChange={e => setPassword
+                    (e.target.value)} value={password} required />
+                </label>
+                <br/><br/>
+                
+                <button className="btn btn-primary" type="submit" onClick={handleSubmit}>Submit</button>
+                
             </div>
         </>)}
 // Login.propTypes = {
