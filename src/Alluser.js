308353ab5e8b48ac1d92cdffca1ff0dff60d6f03
Faisal <tuneinfaisal@gmail.com>
2022-01-16 16:27:42 +0530

    Transaction working

--- src/Alluser.js
+++ src/Alluser.js
@@ -1,8 +1,6 @@
-import React  from "react";
+import React from "react";
 // import Axios from "axios";
 // import { useEffect , useState} from "react";
-
-
 
 // const [repo, setRepo] = useState([]);
 
@@ -15,55 +13,58 @@ import React  from "react";
 // };
 // useEffect (() => getRepo(), []);
 class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-    
-    
-	constructor(props) {
-		super(props);
+        this.state = {
+            items: [],
+            DataisLoaded: false,
+        };
+    }
 
-		this.state = {
-			items: [],
-			DataisLoaded: false
-		};
-	}
+    componentDidMount() {
+        fetch("http://localhost:8000/api/v1/account/users")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true,
+                });
+            });
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded)
+            return (
+                <div>
+                    <h1> Pleses wait some time.... </h1>{" "}
+                </div>
+            );
 
-	componentDidMount() {
-		fetch("http://localhost:8000/api/v1/account/users")
-			.then((res) => res.json())
-			.then((json) => {
-				this.setState({
-					items: json,
-					DataisLoaded: true
-				});
-			})
-	}
-	render() {
-		const { DataisLoaded, items } = this.state;
-		if (!DataisLoaded) return <div>
-			<h1> Pleses wait some time.... </h1> </div> ;
+        return (
+            <>
+                <div className="App">
+                    <h1> Fetched data </h1>{" "}
+                    {items.map((item) => (
+                        <ol key={item.id}>
+                            name: {item.name}, balance: {item.balance}
+                            {/* password: { item.password } */}
+                        </ol>
+                    ))}
+                </div>
 
-		return (
-		<><div className="App">
-                <h1> Fetched data </h1> {items.map((item) => (
-                    <ol key={item.id}>
-                        name: {item.name},
-                        balance: {item.balance}
-                        {/* password: { item.password } */}
-                    </ol>
-                ))}
-            </div>
-            
-            {/* <div classname="App">
+                {/* <div classname="App">
                     <CSVLink {...csvreport} class="button-30">Download data</CSVLink>
                 </div> */}
-                </>
-        
-	);
-}
+            </>
+        );
+    }
 }
 
 export default App;
 
-{/* <div>
+{
+    /* <div>
 <button className="button-30" onClick={getUsers}>Show Users</button>
-</div> */}
+</div> */
+}
--- src/TranPage.js
+++ src/TranPage.js
@@ -1,47 +1,88 @@
-import React from "react";
-import { CSVLink } from "react-csv";
+import React, { useState } from "react";
+import Axios from "axios";
+// import { CSVLink } from "react-csv";
 
-
-//this is DEMO data to test the export CSV file getting saved to system... 
-const headers = [
-    { label: "Name", key: "Name" },
-    { label: "Balance", key: "Balance" },
-    { label: "Password", key: "Password" }
-  ];
-  const data = [
-    { Name: "ankit", Balance: 10000, Password: "abc" },
-    { Name: "nonganba", Balance: 20000, Password: "pqr" },
-    { Name: "faisal", Balance: 30000, Password: "lmn" },
-    { Name: "asif", Balance: 40000, Password: "xyz" }
-  ];
-  const csvreport = {
-    data: data,
-    headers: headers,
-    filename: 'Apna backend ka data.csv'
-  };
-{/* <>
+//this is DEMO data to test the export CSV file getting saved to system...
+// const headers = [
+//     { label: "Name", key: "Name" },
+//     { label: "Balance", key: "Balance" },
+//     { label: "Password", key: "Password" }
+//   ];
+//   const data = [
+//     { Name: "ankit", Balance: 10000, Password: "abc" },
+//     { Name: "nonganba", Balance: 20000, Password: "pqr" },
+//     { Name: "faisal", Balance: 30000, Password: "lmn" },
+//     { Name: "asif", Balance: 40000, Password: "xyz" }
+//   ];
+//   const csvreport = {
+//     data: data,
+//     headers: headers,
+//     filename: 'Apna backend ka data.csv'
+//   };
+/* <>
 <li> Name: "ankit", Balance: 10000, Password: "abc"   </li>
 <li>  Name: "nonganba", Balance: 20000, Password: "pqr"   </li>
 <li> Name: "faisal", Balance: 30000, Password: "lmn"  </li>
-<li> Name: "asif", Balance: 40000, Password: "xyz"   </li></> */}
+<li> Name: "asif", Balance: 40000, Password: "xyz"   </li></> */
 
-const btnhandle = (e) => {
-  e.preventDefault();
-  alert("wait we are fetching your data....");
-}
- function home () {
-    return (<><div class="text-center">
-        <h1>Welcome to Transacation Data</h1>
-    </div><div classname="App">
-            <CSVLink {...csvreport} class="button-30">Download your data</CSVLink>
-        </div>
-        <label><p>Amount</p><input type="number"/></label><br/><br/>
-        <div>
-        <input type="radio" value="Deposit" name="type" /> Deposit
-        <input type="radio" value="Withdraw" name="type" /> Withdraw
-        <br/><br/></div>
-        <button className="button-30" type="submit" onClick={btnhandle}>Proceed</button>
-        
-      </> )}
+export default () => {
 
-export default home;
+    const [name, setName] = useState("Asif");
+    const [amount, setAmount] = useState(0);
+    const [transactionType, setTransactionType] = useState("");
+    // const name = "Asif";
+    const url = `http://localhost:8000/api/v1/account/${name}/transaction`;
+
+    const proceedButtonHandler = (e) => {
+        e.preventDefault();
+        Axios.post(url,{
+            // name: name,
+            amount: amount,
+            transactionType: transactionType
+        })
+        console.log(amount);
+        console.log(transactionType);
+
+        alert("wait we are fetching your data....");
+    };
+
+    return (
+        <>
+            <div class="text-center">
+                <h1>Welcome to Transacation Data</h1>
+            </div>
+            {/* <div classname="App">
+                <CSVLink {...csvreport} class="button-30">
+                    Download your data
+                </CSVLink>
+            </div> */}
+            {/* <label>
+                <p>Name</p>
+                <input type="text"
+                onChange={e => setName
+                (e.target.value)} value={name} required />
+            </label> */}
+            <label>
+                <p>Amount</p>
+                <input type="number"
+                onChange={e => setAmount
+                (e.target.value)} value={amount} required />
+            </label>
+            <br />
+            <br />
+            <div>
+                <input type="radio" name="radio" onChange={e => setTransactionType(e.target.defaultValue)} value={"deposit"} /> Deposit
+                <input type="radio" name="radio" onChange={e => setTransactionType(e.target.defaultValue)} value={"withdraw"} /> Withdraw
+                <br />
+                <br />
+            </div>
+            <button
+                className="button-30"
+                type="submit"
+                onClick={proceedButtonHandler}
+            >
+                Proceed
+            </button>
+        </>
+    );
+};
