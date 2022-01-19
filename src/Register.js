560b79688c85fb899a52044b5e3d3dc06ad52879
Faisal <tuneinfaisal@gmail.com>
2022-01-19 15:13:31 +0530

    Register validation bug fixes

--- src/Register.js
+++ src/Register.js
@@ -1,10 +1,6 @@
 import React, { useState, useRef } from "react";
 import Axios from "axios";
-// import "bootstrap/dist/css/bootstrap.min.css";
-
 import "./Style.css";
-// import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom';
 
 export default function Login() {
     const [name, setName] = useState();
@@ -12,28 +8,46 @@ export default function Login() {
     const [password, setPassword] = useState();
     const url = "http://localhost:8000/api/v1/account/registration"
 
-    // const initialValues = {name:"",balance:"",password:""}
-    // const [formValues, setFormValues] = useState();
+    // const [nameError, setNameError] = useState();
+    // const [balanceError, setBalanceError] = useState();
+    // const [passwordError,setPasswordError] = useState();
+
+    // const valid = () => {
+    //     if(name.includes("")){
+    //         setName("Invalid Name");
+    //     }
+    // }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(name && balance && password) {
-            Axios.post(url,{
-                name: name,
-                balance: balance,
-                password: password
-            })
-            alert("Your data is submitted")
-        }
-        else {
-            alert("Field must not be empty");
-        }
+        Axios.post(url,{
+            name: name,
+            balance: balance,
+            password: password
+        });
+        alert("Successfully registered");
+
+        // this.name = "";
+
+        // if(name && balance && password) {
+        //     Axios.post(url,{
+        //         name: name,
+        //         balance: balance,
+        //         password: password
+        //     })
+        //     alert("Your data is submitted")
+        // }
+
     }
+
+    // const handleChange = (e) => {
+    //     e.preventDefault();
+    // }
     return (
         <>
         <body>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div class="container">
                 <h1>Sign up</h1>
                 <label>
@@ -41,13 +55,16 @@ export default function Login() {
                     <input type="text" placeholder="Enter name"
                     onChange={e => setName(e.target.value)} 
                     value={name} required />
+                    {/* <p>{name}</p> */}
                 </label>
                 <br/>
                 <label>
                     <p>Balance</p>
                     <input type="text" placeholder="Enter balance"
-                    onChange={e => setBalance(e.target.value)} 
+                    onChange={e => setBalance(e.target.value)}
+                    // onChange={handleChange} 
                     value={balance} required />
+                    <p></p>
                 </label>
                 <br/>
                 <label>
@@ -55,10 +72,12 @@ export default function Login() {
                     <input type="password" placeholder="Enter password"
                     onChange={e => setPassword(e.target.value)} 
                     value={password} required />
+                    <p></p>
                 </label>
-                <br/><br/>
+                <br/>
 
-                <button className="btn btn-primary" type="submit" onClick={handleSubmit}>Submit</button>
+                {/* <button className="btn btn-primary" type="submit" onClick={handleSubmit}>Submit</button> */}
+                <button className="btn btn-primary" type="submit" >Submit</button>
                 
             </div>
             </form>
@@ -67,3 +86,71 @@ export default function Login() {
 // Login.propTypes = {
 //     setToken: PropTypes.func.isRequired
 // }
+
+
+
+
+// import PropTypes from 'prop-types';
+// import { Link } from 'react-router-dom';
+
+// export default function Login() {
+//     const [name, setName] = useState();
+//     const [balance, setBalance] = useState();
+//     const [password, setPassword] = useState();
+//     const url = "http://localhost:8000/api/v1/account/registration"
+
+//     // const initialValues = {name:"",balance:"",password:""}
+//     // const [formValues, setFormValues] = useState();
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+
+//         if(name && balance && password) {
+//             Axios.post(url,{
+//                 name: name,
+//                 balance: balance,
+//                 password: password
+//             })
+//             alert("Your data is submitted")
+//         }
+//         else {
+//             alert("Field must not be empty");
+//         }
+//     }
+//     return (
+//         <>
+//         <body>
+//             <form>
+//             <div class="container">
+//                 <h1>Sign up</h1>
+//                 <label>
+//                     <p>Name</p>
+//                     <input type="text" placeholder="Enter name"
+//                     onChange={e => setName(e.target.value)} 
+//                     value={name} required />
+//                 </label>
+//                 <br/>
+//                 <label>
+//                     <p>Balance</p>
+//                     <input type="text" placeholder="Enter balance"
+//                     onChange={e => setBalance(e.target.value)} 
+//                     value={balance} required />
+//                 </label>
+//                 <br/>
+//                 <label>
+//                     <p>Password</p>
+//                     <input type="password" placeholder="Enter password"
+//                     onChange={e => setPassword(e.target.value)} 
+//                     value={password} required />
+//                 </label>
+//                 <br/><br/>
+
+//                 <button className="btn btn-primary" type="submit" onClick={handleSubmit}>Submit</button>
+                
+//             </div>
+//             </form>
+//         </body>
+//         </>)}
+// // Login.propTypes = {
+// //     setToken: PropTypes.func.isRequired
+// // }
